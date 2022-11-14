887e4cc6973cec4392a9b31047eb2405b8d91ff6
Isal <58117968+rfzsal@users.noreply.github.com>
2022-11-14 11:32:45 +0700

    Add polygon api

---
+++ src/pages/api/polygon/files.js
@@ -0,0 +1,23 @@
+import PolygonContract from '../../../../lib/PolygonContract'
+
+export default async function handler(req, res) {
+  try {
+    const contract = PolygonContract.createReadOnly()
+    const count = Number(await contract.r.fileCount())
+
+    const files = []
+    for (let i = count; i >= 1; i--) {
+      const file = await contract.r.files(i)
+      files.push({
+        id: Number(file[0]),
+        hash: file[1],
+        name: file[2]
+      })
+    }
+
+    res.status(200).send({ data: files })
+  } catch (error) {
+    console.log(error)
+    res.status(500).end()
+  }
+}
